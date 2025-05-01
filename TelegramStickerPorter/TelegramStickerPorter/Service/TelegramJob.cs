a8129a43bf87dc67f4db2ee73de4b57cbf770b49
S202502211724\riniba <pulverivqcdl@gmail.com>
2025-05-01 13:30:36 +0800

    默认启动不检测机器人状态

--- TelegramStickerPorter/TelegramStickerPorter/Service/TelegramJob.cs
+++ TelegramStickerPorter/TelegramStickerPorter/Service/TelegramJob.cs
@@ -1,14 +1,14 @@
 ﻿using TelegramStickerPorter;
 
 [JobDetail("job_bot_monitor", Description = "机器人检测", GroupName = "default")]
-[PeriodMinutes(5, TriggerId = "trigger_bot_monitor", Description = "每5分钟检测一次", RunOnStart = true)]
+[PeriodMinutes(5, TriggerId = "trigger_bot_monitor", Description = "每5分钟检测一次", RunOnStart = false)]
 public class TelegramJob : IJob
 {
     private readonly ILogger<TelegramJob> _logger;
     private readonly TelegramBotClientManager _telegramBotClientManager;
 
     public TelegramJob(
-        ILogger<TelegramJob> logger, 
+        ILogger<TelegramJob> logger,
         TelegramBotClientManager telegramBotClientManager)
     {
         _logger = logger;
@@ -19,7 +19,7 @@ public class TelegramJob : IJob
     {
         _logger.LogInformation("检测机器人状态");
         var isAlive = await _telegramBotClientManager.CanPingTelegram();
-        
+
         if (!isAlive)
         {
             _logger.LogInformation("机器人不响应，重新创建中...");
